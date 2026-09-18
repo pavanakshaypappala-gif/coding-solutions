54fafdc7295cf7e4685983d02d412bcb26edcdbb
pavanakshaypappala-gif <pavanakshaypappala@gmail.com>
2026-09-18 21:03:11 +0530

    ✅ Move Zeroes | Easy | Python | Runtime: 3 ms (81.85%) | Memory: 20.4 MB | 2026-09-18

---
+++ leetcode/easy/0283-move-zeroes/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        s = 0
+        for i in range(len(nums)):
+            if nums[i] != 0 :
+                nums[i],nums[s]=nums[s],nums[i]
+                s +=1
+        return nums
+        
