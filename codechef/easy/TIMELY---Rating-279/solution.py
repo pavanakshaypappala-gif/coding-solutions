d76444a9ffdf30a22305e0035d2e47faf82be9b0
pavanakshaypappala-gif <pavanakshaypappala@gmail.com>
2026-09-19 19:25:17 +0530

    ✅ TIMELY - Rating 279 | Easy | Python | Runtime: N/A | Memory: N/A | 2026-09-19

--- codechef/easy/TIMELY---Rating-279/solution.py
+++ codechef/easy/TIMELY---Rating-279/solution.py
@@ -1,8 +1,8 @@
 # cook your dish here
-n = int(input())
-for _ in range(n):
+t = int(input())
+for _ in range(t):
     x = int(input())
-    if x>=30:
+    if 67 <= x <= 45000:
         print("YES")
     else:
         print("NO")
