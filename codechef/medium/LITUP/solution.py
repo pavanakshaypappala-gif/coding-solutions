46c8d03d2e8537408dbeea7a216fafac837c8017
pavanakshaypappala-gif <pavanakshaypappala@gmail.com>
2026-09-23 20:53:57 +0530

    ✅ LITUP | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-23

--- codechef/medium/LITUP/solution.py
+++ codechef/medium/LITUP/solution.py
@@ -2,5 +2,16 @@
 n = int(input())
 for _ in range(n):
     x,y = map(int,input().split())
-    z = map(int,input().split())
+    z = list(map(int,input().split()))
+    a = 10**9
+    for i in range(x):
+        for j in range(i+1,x):
+            l = min(i-y,j-y)
+            r = min(i+y,j+y)
+            if l<=0 and r>=x-1:
+                a = min(a,z[i]+z[j])
+    if a==10**9:
+        print("-1")
+    else:
+        print(a)
     
