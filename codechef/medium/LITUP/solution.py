0446506f0cf9707a3fe3be7b117d6c3b41788a9e
pavanakshaypappala-gif <pavanakshaypappala@gmail.com>
2026-09-23 20:55:06 +0530

    ✅ LITUP | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-23

--- codechef/medium/LITUP/solution.py
+++ codechef/medium/LITUP/solution.py
@@ -7,7 +7,7 @@ for _ in range(n):
     for i in range(x):
         for j in range(i+1,x):
             l = min(i-y,j-y)
-            r = min(i+y,j+y)
+            r = max(i+y,j+y)
             if l<=0 and r>=x-1:
                 a = min(a,z[i]+z[j])
     if a==10**9:
